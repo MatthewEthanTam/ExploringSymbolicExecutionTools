0e53203448321cc3970e64deac57c83905e62411
Matthew Tam <matthewethan.tam@gmail.com>
2022-09-24 15:09:55 +0200

    done

--- CrossHairExample/Test.py
+++ CrossHairExample/Test.py
@@ -5,3 +5,10 @@ def consecutive_pairs(numbers: List[float]) -> float:
     post: min(numbers) <= __return__ <= max(numbers)
     '''
     return sum(numbers) - len(numbers)
+
+def consecutive_pairs2(numbers: List[float]) -> float:
+    ''' 
+    pre: len(numbers) >0
+    post: min(numbers) <= __return__ <= max(numbers)
+    '''
+    return max(numbers) - min(numbers)
